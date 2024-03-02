8b043729a12e0832f647d74bd71b9760aaec4554
Gabriel Zanon <gabrielzanon77@outlook.com>
2024-03-02 17:39:00 -0300

    Configuração Inicial

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vue",
+        "javascript",
+        "typescript",
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "scss",
+        "sql",
+        "vim",
+        "lua",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
