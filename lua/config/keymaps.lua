8aa7728481c0ac6b05553471d15e9e32b2875cfc
Gabriel Zanon <gabrielzanon77@outlook.com>
2024-03-07 14:03:23 -0300

    Removendo keymaps inuteis

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,11 +4,5 @@ local opts = { noremap = true, silent = true }
 keymap.set("n", "x", '"_x')
 keymap.set("i", "jk", "<Esc>")
 
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
--- Save file and quit
 keymap.set("n", "<Leader>w", ":update<Return>", opts)
-keymap.set("n", "<Leader>q", ":quit<Return>", opts)
-keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
+keymap.set("n", "<leader>n", ":nohlsearch<CR>", opts)
